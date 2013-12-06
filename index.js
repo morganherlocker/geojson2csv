4890a2afddabbaa4e1de61748a9e3327896b835b
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-05 19:28:02 -0500

    headers parsed and added

--- index.js
+++ index.js
@@ -2,5 +2,28 @@ var _ = require('lodash')
     fs = require('fs')
 
 module.exports = function(inPath, outPath, done){
-  fs.readFile
+  fs.readFile(inPath, function(err, content){
+    var geojson = JSON.parse(content)
+    var csv = ''
+
+    // add headers
+    _.each(Object.keys(geojson.features[0].properties), function(property){
+      csv += property +','
+    })
+    csv += 'lat,lon'
+    csv += '\n'
+    
+    // add data
+    _.each(geojson.features, function(feature){
+      _.each(feature.properties, function(val){
+
+      })
+      csv += feature.coordinates[0] + ','
+      csv += feature.coordinates[1]
+    })
+
+    // output csv file
+    console.log(csv)
+    done(null, csv)
+  })
 }
--- test/geojson2csv.js
+++ test/geojson2csv.js
@@ -3,7 +3,7 @@ var geojson2csv = require('../index.js'),
 
 describe('geojson2csv', function(){
   it('should take a geojson file and output a csv file', function(done){
-    geojson2shape(__dirname+'/in.geojson', __dirname+'/out.csv', function(err){
+    geojson2csv(__dirname+'/in.geojson', __dirname+'/out.csv', function(err){
       if(err) throw err
       done()
     })
